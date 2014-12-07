b168d8bfcb5d40cc1befa8590f986835ff1039b9
William Guss <carlostnb@gmail.com>
2014-12-06 22:02:14 -0700

    AI and ball

---
+++ core/src/com/lostcodestudios/gnoPortal/gameplay/entities/components/AI.java
@@ -0,0 +1,20 @@
+package com.lostcodestudios.gnoPortal.gameplay.entities.components;
+
+import com.lostcode.javalib.entities.components.Component;
+import com.lostcode.javalib.entities.components.ComponentManager;
+
+public class AI implements Component {
+
+	@Override
+	public void onAdd(ComponentManager container) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onRemove(ComponentManager container) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
